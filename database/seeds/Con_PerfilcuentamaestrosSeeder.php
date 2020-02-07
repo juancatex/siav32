ec45ac5c461b65b3342ea9bbb5f562b04aa8a949
Eddy <eddyclarosc@gmail.com>
2020-02-07 16:58:43 -0400

    modificado seeder de perfil de cuentas

--- database/seeds/Con_PerfilcuentamaestrosSeeder.php
+++ database/seeds/Con_PerfilcuentamaestrosSeeder.php
@@ -35,52 +35,6 @@ class Con_PerfilcuentamaestrosSeeder extends Seeder
             'idmodulo'=>'2'
             
         ]);
-        DB::table('con__perfilcuentamaestros')->insert([
-            'nomperfil'=>'Desembolso Prestamos Regulares',
-            'descripcion'=>'Desembolso Prestamos Regulares',
-            'idtipocomprobante'=>'2',
-            'completo'=>'1',
-            'siporcentaje'=>'0',
-            'idmodulo'=>'3'
-            
-        ]);
-        DB::table('con__perfilcuentamaestros')->insert([
-            'nomperfil'=>'Desembolso Prestamos Emergencia',
-            'descripcion'=>'Desembolso Prestamos Emergencia',
-            'idtipocomprobante'=>'2',
-            'completo'=>'1',
-            'siporcentaje'=>'0',
-            'idmodulo'=>'3' 
-        ]);
-        DB::table('con__perfilcuentamaestros')->insert([
-            'nomperfil'=>'Cobranza Prestamos Emergencia',
-            'descripcion'=>'Cobranza Prestamos Emergencia',
-            'idtipocomprobante'=>'1',
-            'completo'=>'1',
-            'siporcentaje'=>'0',
-            'idmodulo'=>'3' 
-        ]);
-        DB::table('con__perfilcuentamaestros')->insert([
-            'nomperfil'=>'Cobranza Prestamos Regulares',
-            'descripcion'=>'Cobranza Prestamos Regulares',
-            'idtipocomprobante'=>'1',
-            'completo'=>'1',
-            'siporcentaje'=>'0',
-            'idmodulo'=>'3' 
-        ]);
-        DB::table('con__perfilcuentamaestros')->insert([
-            'nomperfil'=>'Desembolso por servicios',
-            'descripcion'=>'Desembolso por servicios',
-            'idtipocomprobante'=>'2',
-            'completo'=>'1',
-            'idmodulo'=>'3' 
-        ]);
-        DB::table('con__perfilcuentamaestros')->insert([
-            'nomperfil'=>'Cobranza por servicios',
-            'descripcion'=>'Cobranza por servicios',
-            'idtipocomprobante'=>'1',
-            'completo'=>'1',
-            'idmodulo'=>'3' 
-        ]);                
+                
     }
 }
