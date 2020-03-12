f1d0394c794ffecc8a56a4bda23a709c504671e8
Your Name <you@example.com>
2020-03-12 11:55:34 -0400

    ok

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '192.168.100.60'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'db_sia'),
             'username' => env('DB_USERNAME', 'root'),
