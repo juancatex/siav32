5f00f87bca6bd76b021e3814e12a22abab56245e
Your Name <you@example.com>
2020-09-23 10:41:42 -0400

    ok

--- app/Glo_SolicitudCargoCuenta.php
+++ app/Glo_SolicitudCargoCuenta.php
@@ -8,5 +8,5 @@ class Glo_SolicitudCargoCuenta extends Model
 {
     protected $table = 'glo__solicitud_cargo_cuentas';
     protected $primaryKey = 'idsolccuenta';
-    protected $fillable = ['idsolccuenta','subcuenta','idusuario','idrole','glosa','monto','estado_aprobado','idasientomaestro','modificado'];
+    protected $fillable = ['idsolccuenta','subcuenta','idusuario','idrole','glosa','monto','estado_aprobado','idasientomaestro','modificado','fecha_desembolso','fecha_apertura_cuenta'];
 }
