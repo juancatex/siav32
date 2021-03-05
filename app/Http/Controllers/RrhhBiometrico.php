63a7fd2e98e5b03e3c3c777ebd5c8d34396a13a8
Your Name <you@example.com>
2021-03-05 10:20:21 -0400

    ad

--- app/Http/Controllers/RrhhBiometrico.php
+++ app/Http/Controllers/RrhhBiometrico.php
@@ -73,7 +73,8 @@ class RrhhBiometrico extends Controller
             // $zk->setUser(62, '49', 'YAHUASI LAURA GONZALO','',Util::LEVEL_USER);
             
             //$zk->setUser(63, '70', 'QUISPE QUISPE YESSICA','',Util::LEVEL_USER);
-            $zk->setUser(64, '71', 'CASTANETA U ANDREA','',Util::LEVEL_USER);
+            // $zk->setUser(64, '71', 'CASTANETA U ANDREA','',Util::LEVEL_USER);
+            $zk->setUser(65, '72', 'LIMACHI URI RAMIRO','',Util::LEVEL_USER);
 
             // $zk->setUser(50, '50', 'admin2','',Util::LEVEL_USER);
             // $zk->setUser(51, '51', 'SOM. ALMANZA WILSON','',Util::LEVEL_USER);
