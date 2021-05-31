54f6d5f678b8434f801a3e005cdbd878290eeb0e
Ing. Juan Carlos <juan_carlos1345@hotmail.com>
2021-05-31 15:33:25 -0400

    ok

--- app/Http/Controllers/ConFacturaController.php
+++ app/Http/Controllers/ConFacturaController.php
@@ -143,12 +143,19 @@ class ConFacturaController extends Controller
 
         if(count($fechaR)>0){
             if($fechaR[0]->tip=='S'){
-                $valida_1=DB::connection($valuedb)->select("select det.analisis_auxiliar,det.id_reg ,cu.descripcion,det.cuenta,det.id_sub_cuenta, det.importe_moneda_local,det.tipo_cambio,p.nombrecompleto, g.abrev 
-                from finanzas.con_tr_detalles  det,finanzas.con_plan_cuentas cu,global.gbpersona p,finanzas.apsa_grados g
-                        where  det.cuenta =cu.cuenta
-                        and p.par_profesion=g.cod
+                // $valida_1=DB::connection($valuedb)->select("select det.analisis_auxiliar,det.id_reg ,cu.descripcion,det.cuenta,det.id_sub_cuenta, det.importe_moneda_local,det.tipo_cambio,p.nombrecompleto, g.abrev 
+                // from finanzas.con_tr_detalles  det,finanzas.con_plan_cuentas cu,global.gbpersona p,finanzas.apsa_grados g
+                //         where  det.cuenta =cu.cuenta
+                //         and p.par_profesion=g.cod
+                //         and det.analisis_auxiliar =cu.analisis_auxiliar
+                //         and det.id_sub_cuenta=p.numero_papeleta
+                // and det.id_transaccion ='$numcomprobante' 
+                // and det.id_tipo ='$valuetipo' order by det.cuenta,det.id_sub_cuenta");
+                $valida_1=DB::connection($valuedb)->select("select det.analisis_auxiliar,det.id_reg ,cu.descripcion,det.cuenta,det.id_sub_cuenta, det.importe_moneda_local,det.tipo_cambio,p.nombrecompleto , '' as abrev
+                from finanzas.con_tr_detalles  det,finanzas.con_plan_cuentas cu,finanzas.con_personas p
+                        where  det.cuenta =cu.cuenta 
                         and det.analisis_auxiliar =cu.analisis_auxiliar
-                        and det.id_sub_cuenta=p.numero_papeleta
+                        and det.id_sub_cuenta=p.id_persona
                 and det.id_transaccion ='$numcomprobante' 
                 and det.id_tipo ='$valuetipo' order by det.cuenta,det.id_sub_cuenta");
                }else{
