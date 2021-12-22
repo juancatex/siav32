f84bab6feaa67cd38c798f2aae029874839bdef0
Ing. juan carlos <juan_carlos1345@hotmail.com>
2021-12-22 12:22:27 -0400

    ok

--- resources/assets/js/components/servicios/Ser_reportes.vue
+++ resources/assets/js/components/servicios/Ser_reportes.vue
@@ -225,7 +225,7 @@
   <div class="modal-dialog modal-dialog-centered modal-primary" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Reporte de hospedaje por socio</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
