2de6893cf4bbd7a613fb4f5900a1de4458be2d6e
Ing. juan carlos <juan_carlos1345@hotmail.com>
2022-03-29 11:31:42 -0400

    ok

--- app/Http/Controllers/apkMovile.php
+++ app/Http/Controllers/apkMovile.php
@@ -19,7 +19,7 @@ class apkMovile extends Controller
     public function validarsaldomenorA(Request $request){
         return DB::connection('pgsql')->select("SELECT sum(importe)*-1 as suma 
         FROM finanzas.ptm_acreedores
-        WHERE fecha_transaccion='2022-03-15' AND id_canal ='AJ_SMEN' and id_persona =?",array($request->socio))[0];
+        WHERE fecha_transaccion='2022-03-15' AND id_canal ='AJ_SMEN' and id_persona =?",array($request->socio))[0]->suma;
     }
     public function validarsaldomenorB(Request $request){
 
