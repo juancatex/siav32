e3fa3fd8992dad979e724ad6789ad59c1450442d
Ing. juan carlos <juan_carlos1345@hotmail.com>
2022-04-04 09:39:33 -0400

    tf

--- app/Http/Controllers/apkMovile.php
+++ app/Http/Controllers/apkMovile.php
@@ -24,7 +24,7 @@ class apkMovile extends Controller
     public function validarsaldomenorB(Request $request){
         return DB::connection('pgsql')->select("SELECT *
         FROM finanzas.ptm_tr_contable
-        WHERE id_prestamo =? and id_comprobante='222' and contabilizado ='S' and cuenta='51301111' limit 1",array($request->idprestamo))[0];
+        WHERE id_prestamo =? and id_comprobante='222' and contabilizado ='S' and cuenta='51301111' limit 1",array($request->idprestamo));
     }
     public function gethtml()
     {
