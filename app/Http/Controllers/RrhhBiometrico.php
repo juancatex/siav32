e03692ec8f7ed0e39e4ad8a4884bff8dfb1ec5e4
Ing. juan carlos <juan_carlos1345@hotmail.com>
2022-06-06 13:40:20 -0400

    ok

--- app/Http/Controllers/RrhhBiometrico.php
+++ app/Http/Controllers/RrhhBiometrico.php
@@ -120,7 +120,11 @@ class RrhhBiometrico extends Controller
             // $zk->setUser(107, '114', 'ANAHI VARGAS','',Util::LEVEL_USER);
             // $zk->setUser(108, '115', 'JHAQUELINE APAZA','',Util::LEVEL_USER);
             // $zk->setUser(109, '116', 'LEILA VALENCIA','',Util::LEVEL_USER);
-            $zk->setUser(110, '117', 'CRISTHIAN CACERES','',Util::LEVEL_USER);
+            // $zk->setUser(110, '117', 'CRISTHIAN CACERES','',Util::LEVEL_USER);
+            $zk->setUser(111, '118', 'ALBERTO QUISPE','',Util::LEVEL_USER);
+            $zk->setUser(112, '119', 'WENDY VILLARROEL','',Util::LEVEL_USER);
+            $zk->setUser(113, '120', 'WILMA YAPURA','',Util::LEVEL_USER);
+
 
    
             // $zk->setUser(50, '50', 'admin2','',Util::LEVEL_USER);
