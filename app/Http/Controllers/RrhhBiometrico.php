3f9a8c81bcad971e42ad0c20ffcc7652f2f5ced7
Ing. Juan Carlos Colque <juan_carlos1345@hotmail.com>
2022-07-19 10:01:15 -0400

    ok

--- app/Http/Controllers/RrhhBiometrico.php
+++ app/Http/Controllers/RrhhBiometrico.php
@@ -127,7 +127,8 @@ class RrhhBiometrico extends Controller
             // $zk->setUser(114, '121', 'CAROLINA MEAVE','',Util::LEVEL_USER);
             // $zk->setUser(115, '122', 'FREDY QUISPE ANTEZANA','',Util::LEVEL_USER);
             // $zk->setUser(116, '123', 'BRANDON OROSCO','',Util::LEVEL_USER);
-            $zk->setUser(117, '124', 'LUIS VARGAS ALVAREZ','',Util::LEVEL_USER);
+            // $zk->setUser(117, '124', 'LUIS VARGAS ALVAREZ','',Util::LEVEL_USER);
+            $zk->setUser(118, '125', 'DIEGO VILLCA ROQUE','',Util::LEVEL_USER);
 
    
             // $zk->setUser(50, '50', 'admin2','',Util::LEVEL_USER);
