7c0a8cd601fa0d1ef4336b1a90ef0c44b5e682b2
Ing. Juan Carlos Colque <juan_carlos1345@hotmail.com>
2022-08-23 10:14:36 -0400

    ok

--- app/Http/Controllers/apkMovile.php
+++ app/Http/Controllers/apkMovile.php
@@ -22,7 +22,9 @@ class apkMovile extends Controller
         $prestamos = DB::connection('pgsql')->select("SELECT m.par_automatico , d.id_transaccion,d.id_tipo  
         FROM finanzas.con_tr_detalles d, finanzas.con_tr_maestro m
         where d.id_transaccion =m.id_transaccion and d.id_tipo =m.id_tipo 
-        and ( d.cuenta='41101101' or  d.cuenta='41101103')  and m.fecha_revmod is null and m.id_transaccion_revmod is null group by 1,2,3 order by 1,2");
+        and ( d.cuenta='41101101' or  d.cuenta='41101103') 
+        and (d.fecha_transaccion >='2022-01-01' and d.fecha_transaccion <='2022-01-31') 
+        and m.fecha_revmod is null and m.id_transaccion_revmod is null group by 1,2,3 order by 1,2");
         $sumacuentadaaro=0;
         $sumasincuentadaaro=0;
         $ivacon1=0;
